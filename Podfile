59ac9edfabff3e44b0d5725e0e1e11f3ed9ce19c
xunan <xunan623@163.com>
2017-04-11 17:38:05 +0800

    初始化项目 爱玩

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'XNLovePlay_Swift' do
+
+pod 'Alamofire', '~> 4.4.0'
+
+end
---
+++ Pods/Target Support Files/Pods-XNLovePlay_Swift/Pods-XNLovePlay_Swift-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_XNLovePlay_SwiftVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_XNLovePlay_SwiftVersionString[];
+
